70934f72a17000aed566d78bf9c430d99b316ac0
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2021-11-21 09:48:10 -0300

    Tratamento de excecao

--- estrutura-trycatch/src/application/Program.java
+++ estrutura-trycatch/src/application/Program.java
@@ -7,19 +7,39 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		method2();
+
+		System.out.println("End of program");
+
+	}
+
+	public static void method1() {
+		System.out.println("****METHOD1 START****");
+		method2();
+		System.out.println("****METHOD1 END****");
+
+	}
+
+	public static void method2() {
+
+		System.out.println("****METHOD2 START****");
 		Scanner sc = new Scanner(System.in);
 
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
-		} catch (ArrayIndexOutOfBoundsException e) {
+		} 
+		
+		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
-		} catch (InputMismatchException e) {
+			e.printStackTrace();
+			sc.next();
+		} 
+		
+		catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
-
-		System.out.println("End of program");
 
 		sc.close();
 	}

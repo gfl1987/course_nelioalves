0ce2bddfb55ac8d154865b2f085af45a033c5a93
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2022-01-03 18:11:05 -0300

    Consumer exemplo com forEach

---
+++ consumerComForEach/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		//declarado um valor para variável factor
+		double factor = 1.1;
+		
+		// Percorre a lista e atualiza o preço de acordo com o método declarado
+		// na classe PriceUpdate
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+
+		list.forEach(System.out::println);
+	}
+}
